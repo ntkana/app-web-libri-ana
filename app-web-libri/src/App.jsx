4b12c62245d7c737fbf756a2f181c90e4791c4ab
AnaLe <analuizaalg110@gmail.com>
2025-05-02 19:35:57 -0300

    Criação do formulário de cadastro.

--- app-web-libri/src/App.jsx
+++ app-web-libri/src/App.jsx
@@ -2,28 +2,43 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+ 
 import NavBar from './components/layouts/NavBar'
 import Container from './components/layouts/Container'
 import Home from './components/pages/Home'
 import Createbooks from './components/pages/CreateBook'
 import ListBooks from './components/pages/Listbooks'
-
+ 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+ 
 function App() {
   const [count, setCount] = useState(0)
-
+ 
   return (
     <>
-
-       <Container>
-         <NavBar />
-         <Home />
-         <Createbooks />
-         <ListBooks />
-       </Container>
-
+ 
+      {/* <Container>
+        <NavBar />
+        <Home />
+        <Createbooks />
+        <ListBooks />
+      </Container> */}
+ 
+      <BrowserRouter>
+        <Container>
+            <Routes>
+              <Route path='/' element={<NavBar />}>
+                  <Route path='/' element={<Home />} />
+                  <Route path='/listBook' element={<ListBooks />} />
+                  <Route path='/newBook' element={<Createbooks />} />
+              </Route>
+            </Routes>
+        </Container>
+      </BrowserRouter>
+ 
+       
     </>
   )
 }
-
+ 
 export default App
